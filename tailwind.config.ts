d0cb3c422f077b17f7db99c8256b967ef119dcfd
Htet Aung Lin <htetlin099798@gmail.com>
2024-08-27 00:57:44 +0630

    chore: add images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+      },
       colors: {
         background: "#F1F5F9",
         foreground: "#000000",

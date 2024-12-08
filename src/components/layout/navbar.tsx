9eb918e70a5e97edef08155c13e0135870d06ae2
HTLA380 <htetlin099798@gmail.com>
2024-12-08 18:31:57 +0630

    chore: replace svg image with component

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import Button from "../mijn-ui/Button";
-import Image from "next/image";
+import ThemeToggler from "../theme-toggler";
+import Logo from "../icons/logo";
 
 const Navbar = () => {
   return (
@@ -9,25 +9,18 @@ const Navbar = () => {
       <nav className="flex w-full max-w-screen-xl items-center justify-between px-5 py-2.5 sm:p-5">
         <Link href={"/"} className="flex items-center gap-2">
           <span className="w-6 sm:w-8">
-            <Image
-              src={"/assets/logo.svg"}
-              width={120}
-              height={45}
-              alt="logo"
-            />
+            <Logo className="size-7" />
           </span>
 
           <p className="text-base font-bold tracking-wide sm:text-xl">MijnUI</p>
         </Link>
 
-        <Button
-          renderAs="a"
-          href="https://mijnui.com/docs"
-          target="_blank"
-          className="bg-black text-xs hover:bg-black/80 sm:text-sm"
-        >
-          Documentation
-        </Button>
+        <ThemeToggler
+          variant={"text"}
+          color="accent"
+          radius={"full"}
+          className="text-lg"
+        />
       </nav>
     </header>
   );

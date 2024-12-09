900153b4e60cd64ccd1ad8311480af40f7a7125e
HTLA380 <htetlin099798@gmail.com>
2024-12-09 15:33:39 +0630

    style: update tailwind config and css files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@mijn-ui/**/dist/*.js",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       screens: {

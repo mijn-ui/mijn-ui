77fbdfd4300a1d96ffed406eee79e8aaa7cf72eb
HTLA380 <htetlin099798@gmail.com>
2024-12-09 19:37:36 +0630

    build: config nextjs multizone

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/react",
+        destination: `${process.env.REACT_DOMAIN}`,
+      },
+      {
+        source: "/react/:path+",
+        destination: `${process.env.REACT_DOMAIN}/:path+`,
+      },
+      {
+        source: "/tailwind",
+        destination: `${process.env.TAILWIND_DOMAIN}`,
+      },
+      {
+        source: "/tailwind/:path+",
+        destination: `${process.env.TAILWIND_DOMAIN}/:path+`,
+      },
+    ]
+  },
 }
 
 export default nextConfig

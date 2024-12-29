4532511164f02ba8994162eee750ae547b5833f4
HTLA380 <htetlin099798@gmail.com>
2024-12-30 00:30:58 +0630

    chore: update public url and docker file

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/react",
-        destination: `${process.env.REACT_DOMAIN}`,
-      },
-      {
-        source: "/react/:path+",
-        destination: `${process.env.REACT_DOMAIN}/:path+`,
-      },
-      {
-        source: "/tailwind",
-        destination: `${process.env.TAILWIND_DOMAIN}`,
-      },
-      {
-        source: "/tailwind/:path+",
-        destination: `${process.env.TAILWIND_DOMAIN}/:path+`,
-      },
-    ]
-  },
 }
 
 export default nextConfig
--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -22,6 +22,6 @@ export function createMetadata(override: Metadata): Metadata {
 }
 
 export const baseUrl =
-  process.env.NODE_ENV === "development" || !process.env.VERCEL_URL
+  process.env.NODE_ENV === "development" || !process.env.NEXT_PUBLIC_APP_URL
     ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_URL}`)
+    : new URL(`https://${process.env.NEXT_PUBLIC_APP_URL}`)
